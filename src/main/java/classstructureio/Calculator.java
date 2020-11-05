fbe2a264bde6ffab192d1f5e9a96941120df08b7
Hatvani-1966 <hatvaniattila@t-online.hu>
2020-11-05 19:49:02 +0100

    completed variables

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -1,4 +1,15 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int k = 0;
+        i = scanner.nextInt();
+        k = scanner.nextInt();
+        System.out.println(i+" + "+k);
+        System.out.println(i+k);
+    }
 }
