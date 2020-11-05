e068d06b966f89a717e1512745365026e2dd4762
Hatvani-1966 <hatvaniattila@t-online.hu>
2020-11-05 20:11:44 +0100

    finished Registration

--- src/main/java/classstructureio/Registration.java
+++ src/main/java/classstructureio/Registration.java
@@ -1,4 +1,17 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Registration {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("type yoyr username");
+        String userName =  scanner.nextLine();
+        System.out.println("type your e-mail");
+        String email =  scanner.nextLine();
+        System.out.println("username: " + userName );
+        System.out.println("email " + email );
+
+    }
 }
